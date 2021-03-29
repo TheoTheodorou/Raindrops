3f975b5d62333cefe4b16b06949aa2fe53cc3c1c
Theo Theodorou <theodore.theodorou@mac.com>
2021-03-29 10:09:10 +0100

    Added comments and validation

--- Raindrops/Raindrops/Program.cs
+++ Raindrops/Raindrops/Program.cs
@@ -7,39 +7,57 @@ namespace RaindropsApp
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a number: ");
-            int n = Int32.Parse(Console.ReadLine());
+            bool success = Int32.TryParse(Console.ReadLine(),out int n);
 
-            Console.WriteLine(PlingPlangPlong(n));
+            if (success)
+            {
+                Console.WriteLine(PlingPlangPlong(n));
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
         }
 
+        /// <summary>
+        /// Method to convert a number to a string, depending on the number's factors
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>
+        /// Output string
+        /// </returns>
         public static string PlingPlangPlong(int x)
         {
+            // Empty string
             string output = "";
 
+            // If the number has a factor of 3, output 'Pling'
             if (x % 3 == 0)
             {
                 output += "Pling";
             }
 
+            // If the number has a factor of 5, output 'Plang'
             if (x % 5 == 0)
             {
                 output += "Plang";
             }
 
+            // If the number has a factor of 7, output 'Plong'
             if (x % 7 == 0)
             {
                 output += "Plong";
             }
 
-            if(output == "")
+            // If the number does not have any of the above as a factor simply return the numbers digits 
+            if (output == "")
             {
                 output += x;
             }
 
             return output;
-
-
-
         }
     }
 }
